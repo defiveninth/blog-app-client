04f489bca1b6f92ac200795e25077bd56ea83663
XBDURRXUF I <misterfighter1990@gmail.com>
2025-03-27 23:48:39 +0500

    stricted /auth page for authorized users

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+	const AUTH = request.cookies.has('authToken')
+	const PATH = request.nextUrl.pathname
+
+	if (PATH.startsWith('/auth') && !AUTH) {
+		return NextResponse.redirect(new URL('/', request.url))
+	}
+
+	return NextResponse.next()
+}
+
+export const config = {
+	matcher: ['/auth/:path*'],
+}
